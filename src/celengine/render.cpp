b8d0e73dd18421a7e94ec1bb0723d6ab9a328247
pirogronian <pirogronian0@gmail.com>
2019-04-27 19:31:36 +0200

    Added debug octree stats, visible together with fps counter.

--- src/celengine/render.cpp
+++ src/celengine/render.cpp
@@ -6749,6 +6749,9 @@ void Renderer::renderPointStars(const AstroDatabase& aDB,
     else
         starRenderer.starVertexBuffer->startSprites();
 
+    m_starProcStats.objects = 0;
+    m_starProcStats.height = 0;
+    m_starProcStats.nodes = 0;
     processVisibleStars(
         aDB.getOctree(),
         starRenderer,
@@ -6756,7 +6759,8 @@ void Renderer::renderPointStars(const AstroDatabase& aDB,
         observer.getOrientationf(),
         degToRad(fov),
         windowWidth / windowHeight,
-        faintestMagNight);
+        faintestMagNight,
+        &m_starProcStats);
     /*starDB.findVisibleStars(starRenderer,
                             obsPos.cast<float>(),
                             observer.getOrientationf(),
@@ -6996,7 +7000,7 @@ void Renderer::renderDeepSkyObjects(const Universe&  universe,
     dsoRenderer.pixelSize        = pixelSize;
     dsoRenderer.brightnessScale  = brightnessScale * corrFac;
     dsoRenderer.brightnessBias   = brightnessBias;
-    dsoRenderer.avgAbsMag        = 0;
+    dsoRenderer.avgAbsMag        = 6;
     dsoRenderer.faintestMag      = faintestMag;
     dsoRenderer.faintestMagNight = faintestMagNight;
     dsoRenderer.saturationMag    = saturationMag;
@@ -7028,6 +7032,9 @@ void Renderer::renderDeepSkyObjects(const Universe&  universe,
 
     glBlendFunc(GL_SRC_ALPHA, GL_ONE);
 
+    m_dsoProcStats.objects = 0;
+    m_dsoProcStats.height = 0;
+    m_dsoProcStats.nodes = 0;
     processVisibleDsos(
         aDB->getOctree(),
         dsoRenderer,
@@ -7035,7 +7042,8 @@ void Renderer::renderDeepSkyObjects(const Universe&  universe,
         observer.getOrientationf(),
         degToRad(fov),
         windowWidth / windowHeight,
-        2 * faintestMagNight);
+        2 * faintestMagNight,
+        &m_dsoProcStats);
 
     // clog << "DSOs processed: " << dsoRenderer.dsosProcessed << endl;
 
