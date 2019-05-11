4b342cdd65a51b41a6634d72d297ac7c729a0bd4
pirogronian <pirogronian0@gmail.com>
2019-05-11 21:21:34 +0200

    Put octree debug code into #ifdef & some cleanups.

--- src/celengine/render.cpp
+++ src/celengine/render.cpp
@@ -6749,11 +6749,13 @@ void Renderer::renderPointStars(const AstroDatabase& aDB,
     else
         starRenderer.starVertexBuffer->startSprites();
 
+#ifdef OCTREE_DEBUG
     m_starProcStats.reset();
     m_starProcStats.selection = m_selected;
     m_starProcStats.obsPos = obsPos;
     m_starProcStats.limit = faintestMagNight;
     create5FrustumPlanes(m_starProcStats.frustPlanes, obsPos, observer.getOrientationf(), degToRad(fov), windowWidth / windowHeight);
+#endif
     processVisibleStars(
         aDB.getStarOctree(),
         starRenderer,
@@ -6761,15 +6763,12 @@ void Renderer::renderPointStars(const AstroDatabase& aDB,
         observer.getOrientationf(),
         degToRad(fov),
         windowWidth / windowHeight,
+#ifdef OCTREE_DEBUG
         faintestMagNight,
         &m_starProcStats);
-    /*starDB.findVisibleStars(starRenderer,
-                            obsPos.cast<float>(),
-                            observer.getOrientationf(),
-                            degToRad(fov),
-                            (float) windowWidth / (float) windowHeight,
-                            faintestMagNight);
-                            */
+#else
+        faintestMagNight);
+#endif
 
     starRenderer.starVertexBuffer->render();
     starRenderer.glareVertexBuffer->render();
@@ -7034,9 +7033,11 @@ void Renderer::renderDeepSkyObjects(const Universe&  universe,
 
     glBlendFunc(GL_SRC_ALPHA, GL_ONE);
 
+#ifdef OCTREE_DEBUG
     m_dsoProcStats.reset();
     m_dsoProcStats.selection = m_selected;
     m_dsoProcStats.limit = 2 * faintestMagNight;
+#endif
     processVisibleDsos(
         aDB->getDsoOctree(),
         dsoRenderer,
@@ -7044,8 +7045,12 @@ void Renderer::renderDeepSkyObjects(const Universe&  universe,
         observer.getOrientationf(),
         degToRad(fov),
         windowWidth / windowHeight,
+#ifdef OCTREE_DEBUG
         2 * faintestMagNight,
         &m_dsoProcStats);
+#else
+        2 * faintestMagNight);
+#endif
 
     // clog << "DSOs processed: " << dsoRenderer.dsosProcessed << endl;
 
